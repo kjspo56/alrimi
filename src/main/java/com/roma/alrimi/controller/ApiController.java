f2fc1592a7cff7ca952f52d8f520fc177450a078
kjspo56 <kjspo56@gmail.com>
2022-12-17 17:53:09 +0900

    getAPI Test

--- src/main/java/com/roma/alrimi/controller/ApiController.java
+++ src/main/java/com/roma/alrimi/controller/ApiController.java
@@ -1,6 +1,9 @@
 package com.roma.alrimi.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +50,73 @@ public class ApiController {
         }
         return result + "";
     }
+
+        @GetMapping("/read")
+        public void getJsonInfo(){
+            System.out.println("Get JSON Data");
+
+            // 인증키 (개인이 받아와야함)
+            String key = "";
+            // 파싱한 데이터를 저장할 변수
+            String result = "";
+
+            try {
+                URL url = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key="
+                        + key + "&movieCd=20124039");
+
+                BufferedReader bf;
+
+                bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+
+                result = bf.readLine();
+
+                JSONParser jsonParser = new JSONParser();
+                JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
+                JSONObject movieInfoResult = (JSONObject)jsonObject.get("movieInfoResult");
+                JSONObject movieInfo = (JSONObject)movieInfoResult.get("movieInfo");
+
+                JSONArray nations = (JSONArray)movieInfo.get("nations");
+                JSONObject nations_nationNm = (JSONObject)nations.get(0);
+
+                JSONArray directors = (JSONArray)movieInfo.get("directors");
+                JSONObject directors_peopleNm = (JSONObject)directors.get(0);
+
+                JSONArray genres = (JSONArray)movieInfo.get("genres");
+
+                JSONArray actors = (JSONArray)movieInfo.get("actors");
+
+                System.out.println("영화코드 : " + movieInfo.get("movieCd"));
+                System.out.println("영화명(한글) : " + movieInfo.get("movieNm"));
+                System.out.println("영화명(영문) : " + movieInfo.get("movieNmEn"));
+                System.out.println("재생시간 : " + movieInfo.get("showTm"));
+                System.out.println("개봉일 : " + movieInfo.get("openDt"));
+                System.out.println("영화유형 : " + movieInfo.get("typeNm"));
+                System.out.println("제작국가명 : " + nations_nationNm.get("nationNm"));
+
+                String genreNm = "";
+
+                for(int i = 0; i < genres.size(); i++) {
+                    JSONObject genres_genreNm = (JSONObject)genres.get(i);
+                    genreNm += genres_genreNm.get("genreNm") + " ";
+                }
+
+                System.out.println("장르 : " + genreNm);
+
+                System.out.println("감독명 : " + directors_peopleNm.get("peopleNm"));
+
+                String peopleNm = "";
+
+                for(int i = 0; i < actors.size(); i++) {
+                    JSONObject actors_peopleNm = (JSONObject)actors.get(i);
+                    peopleNm += actors_peopleNm.get("peopleNm") + " ";
+                    peopleNm = peopleNm + actors_peopleNm.get("peopleNm") + " ";
+                }
+
+                System.out.println("출연배우 : " + peopleNm);
+
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 }
---
+++ src/test/java/com/roma/alrimi/service/infoTest.java
@@ -0,0 +1,23 @@
+package com.roma.alrimi.service;
+
+import org.junit.jupiter.api.Test;
+
+public class infoTest {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+    //public info read to JSON data
+
+    //check JSON data for Tennis reservation
+
+    //refact data for comfort JSON data
+
+    //get wanted data -> send to sms message or kakaotalk info message
+    //save the history of database(postgres)
+
+    //result : send check public info schedule batch api
+
+
+}
